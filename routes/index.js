7154e007e90f90e1ba0361e3dd64b8d0f1ff710d
Phaser <piotr.piszczek666@gmail.com>
2022-01-01 23:56:05 +0100

    account creation and simple auth

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { body } = require('express-validator')
 const router = express.Router()
 const user_controller = require('../controllers/user')
 
@@ -6,6 +7,16 @@ router.get('/', (req, res) => {
     res.render('index', { title: 'Bank' })
 })
 
-router.post('/', user_controller.create)
+router.post(
+    '/', body('email').isEmail(),
+    body('password').isLength({ min: 8, max: 32 }),
+    user_controller.create
+)
+
+router.post(
+    '/login', body('email').isEmail(),
+    body('password').isLength({ max: 32 }),
+    user_controller.login
+)
 
 module.exports = router
